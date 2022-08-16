1728fcc3c3982d00795a7ad96b504674d77bba81
Damian Montero <photoca@hotmail.com>
2022-08-16 15:15:03 -0400

    functions

--- index.ts
+++ index.ts
@@ -1,1 +1,59 @@
-console.log("Hello automatic typescript")
+let firstName:string = "damian"
+let age: number = 49
+let active: boolean = true
+
+// let names: string[] = [1,2,3,4] // gives us an error
+let names: string[] = ["bobby","jennifer","lily"] // good array
+
+
+enum studentTypes {
+    pending,    // 0
+    active,     // 1
+    almostdone, // 2
+    presenting, // 3
+    graduated,  // 4
+    tas,        // 5
+    teachers    // 6
+}
+
+let mystatus = studentTypes.graduated
+
+// switch (Number(mystatus)) {
+//     case Number(studentTypes.active): 
+//             console.log(" student is active ")
+//         break;
+//     case Number(studentTypes.graduated): 
+//             console.log("Student is graduated")
+//         break;
+//     default: 
+//             console.log("🤔")
+// }
+
+
+// console.log(studentTypes)
+
+// console.log("mystatus is",mystatus)
+// if (mystatus == studentTypes.graduated) {
+//  console.log(`the student is graduated`)
+// }
+
+
+// console.log(names)
+// console.log(`my firstname is ${firstName}`)
+
+
+// let code: string | number  = 11
+// code = "aa11"
+// // code = false // error. not a string or number
+
+// console.log(`my entry code is: ${code}`)
+
+function sum(arr: number[]): number {
+    return arr.length
+    //return arr.reduce((total: number,num: number) => total + num)
+   // return arr.reduce( (previousValue: number,currentValue:number)=> { return previousValue + currentValue} )
+}
+
+let numbers: number[] = [1,2,3,4,5,6]
+
+console.log(sum(numbers))
